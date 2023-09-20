6a194b8eef517fcd297d2dae3049eb5740754a3d
Brandon <blor15@hotmail.com>
2023-09-19 19:47:42 -0600

    player displayed on the board

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ canvas.height = window.innerHeight;
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// Player functionality
+class Player {
+  constructor({ position, velocity }) {
+    this.position = position; // {x,y}
+    this.velocity = velocity;
+  }
+
+  draw() {
+    // Checks that the player is in the center
+    /* ctx.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    */
+    // Rectangle
+    // ctx.fillStyle = "red";
+    // ctx.fillRect(this.position.x, this.position.y, 50, 50);
+
+    ctx.moveTo(this.position.x + 30, this.position.y);
+    ctx.lineTo(this.position.x - 10, this.position.y - 10);
+    ctx.lineTo(this.position.x - 10, this.position.y + 10);
+    ctx.closePath();
+
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+  }
+}
+
+// Default for the player
+const player = new Player({
+  position: { x: canvas.width / 2, y: canvas.height / 2 },
+  velocity: { x: 0, y: 0 },
+});
+
+player.draw();
