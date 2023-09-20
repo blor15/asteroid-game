6157d6404b5c3f0e9dabd2c25d000dc5120e8ed8
Brandon <blor15@hotmail.com>
2023-09-19 20:28:35 -0600

    player movement added

--- script.js
+++ script.js
@@ -15,18 +15,25 @@ class Player {
   constructor({ position, velocity }) {
     this.position = position; // {x,y}
     this.velocity = velocity;
+    this.rotation = 0;
   }
 
   draw() {
-    // Checks that the player is in the center
-    /* ctx.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
+    ctx.save();
+
+    ctx.translate(this.position.x, this.position.y);
+    ctx.rotate(this.rotation);
+    ctx.translate(-this.position.x, -this.position.y);
+
+    ctx.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
     ctx.fillStyle = "blue";
     ctx.fill();
-    */
+
     // Rectangle
     // ctx.fillStyle = "red";
     // ctx.fillRect(this.position.x, this.position.y, 50, 50);
 
+    ctx.beginPath();
     ctx.moveTo(this.position.x + 30, this.position.y);
     ctx.lineTo(this.position.x - 10, this.position.y - 10);
     ctx.lineTo(this.position.x - 10, this.position.y + 10);
@@ -34,6 +41,13 @@ class Player {
 
     ctx.strokeStyle = "white";
     ctx.stroke();
+    ctx.restore();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
   }
 }
 
@@ -43,4 +57,75 @@ const player = new Player({
   velocity: { x: 0, y: 0 },
 });
 
-player.draw();
+// Player movement
+
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+window.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      console.log("w was pressed");
+      keys.w.pressed = true;
+      break;
+    case "KeyA":
+      console.log("a was pressed");
+      keys.a.pressed = true;
+      break;
+    case "KeyD":
+      console.log("d was pressed");
+      keys.d.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "KeyW":
+      console.log("w was pressed");
+      keys.w.pressed = false;
+      break;
+    case "KeyA":
+      console.log("a was pressed");
+      keys.a.pressed = false;
+      break;
+    case "KeyD":
+      console.log("d was pressed");
+      keys.d.pressed = false;
+      break;
+  }
+});
+
+const SPEED = 3;
+const ROTATIONAL_SPEED = 0.05;
+const FRICTION = 0.97;
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.update();
+
+  if (keys.w.pressed) {
+    player.velocity.x = Math.cos(player.rotation) * SPEED;
+    player.velocity.y = Math.sin(player.rotation) * SPEED;
+  } else if (!keys.w.pressed) {
+    player.velocity.x *= FRICTION;
+    player.velocity.y *= FRICTION;
+  }
+
+  if (keys.d.pressed) player.rotation += ROTATIONAL_SPEED;
+  else if (keys.a.pressed) player.rotation -= ROTATIONAL_SPEED;
+}
+
+animate();
