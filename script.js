ab2d892fdbcdec620752de7b1fb66badd9afe5ac
Brandon <blor15@hotmail.com>
2023-09-20 19:34:41 -0600

    projectile functionality

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const canvas = document.querySelector("canvas");
 // Context on the canvas
 const ctx = canvas.getContext("2d");
 
+// CONSTANT
+const SPEED = 3;
+const ROTATIONAL_SPEED = 0.05;
+const FRICTION = 0.97;
+const PROJECTILES_SPEED = 5;
+
+const projectiles = [];
+
 // Takes the whole window width and height on the user device instead of the default 300 x 150
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -57,6 +65,36 @@ const player = new Player({
   velocity: { x: 0, y: 0 },
 });
 
+// Projectile functionality
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 5;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(
+      this.position.x,
+      this.position.y,
+      this.radius,
+      0,
+      Math.PI * 2,
+      false
+    );
+    ctx.closePath();
+    ctx.fillStyle = "white";
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 // Player movement
 
 const keys = {
@@ -85,6 +123,19 @@ window.addEventListener("keydown", (event) => {
       console.log("d was pressed");
       keys.d.pressed = true;
       break;
+    case "Space":
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + Math.cos(player.rotation) * 30,
+            y: player.position.y + Math.sin(player.rotation) * 30,
+          },
+          velocity: {
+            x: Math.cos(player.rotation * PROJECTILES_SPEED),
+            y: Math.sin(player.rotation * PROJECTILES_SPEED),
+          },
+        })
+      );
   }
 });
 
@@ -105,16 +156,27 @@ window.addEventListener("keyup", (event) => {
   }
 });
 
-const SPEED = 3;
-const ROTATIONAL_SPEED = 0.05;
-const FRICTION = 0.97;
-
 function animate() {
   window.requestAnimationFrame(animate);
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   player.update();
+
+  for (let i = projectiles.length - 1; i >= 0; i--) {
+    const projectile = projectiles[i];
+    projectile.update();
+
+    //
+    if (
+      projectile.position.x + projectile.radius < 0 ||
+      projectile.position.x - projectile.radius > canvas.width ||
+      projectile.position.y - projectile.radius > canvas.height ||
+      projectile.position.y + projectile.radius < 0
+    ) {
+      projectiles.splice(i, 1);
+    }
+  }
 
   if (keys.w.pressed) {
     player.velocity.x = Math.cos(player.rotation) * SPEED;
